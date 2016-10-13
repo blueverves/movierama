437b1d7900e3f39bb546fc6de4be127551077980
blueverves <volker.rabe@gmail.com>
2016-10-13 10:20:16 +0100

    add email as attribute to user

--- app/services/voting_booth.rb
+++ app/services/voting_booth.rb
@@ -7,16 +7,20 @@ class VotingBooth
 
   def vote(like_or_hate)
     set = case like_or_hate
-      when :like then @movie.likers
-      when :hate then @movie.haters
-      else raise
-    end
+          when :like then
+            @movie.likers
+          when :hate then
+            @movie.haters
+          else raise
+          end
     unvote # to guarantee consistency
     set.add(@user)
+    Rails.logger.debug "VotingBooth.vote: new #{like_or_hate} for #{@movie.title}"
+    Resque.enqueue(Vote, @movie.to_param, like_or_hate)
     _update_counts
     self
   end
-  
+
   def unvote
     @movie.likers.delete(@user)
     @movie.haters.delete(@user)
